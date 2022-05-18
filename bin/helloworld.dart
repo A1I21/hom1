81f4d4fe177e6e4fdb9dec1420dee0c746d33ed0
A1I21 <87023979+A1I21@users.noreply.github.com>
2022-05-18 17:46:07 +0300

    myhomwork

---
+++ bin/helloworld.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+void main() {
+
+  
+  //my phone number
+  var phonenum="0594909359";
+  //my name
+  var name="Ali Alharbi" ;
+  //my date of birth 
+  const BD="2000";
+print(' my name is $name my phone number: $phonenum my birth date $BD ');
+print(phonenum[4]);
+print(name.length);
+//vv
+
+
+}
+
